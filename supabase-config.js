fa17507bc2d61883a8c8ff7791bb36711f166054
TAMIZHARASAN R <jefftamizh01@gmail.com>
2025-07-19 19:27:24 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,8 @@
 // Supabase Configuration
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cmriyjrqkvpdchvbpnne.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtcml5anJxa3ZwZGNodmJwbm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MzcyODYsImV4cCI6MjA2ODQxMzI4Nn0.wWRO5jZuUfrMPV8A3J7j36yweLe4o-uIcSZYaMhY4O8'
+const supabaseUrl = ' X ' 
+const supabaseAnonKey = ' X '
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
